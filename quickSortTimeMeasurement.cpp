19b9c19e5308c79741f056a5f425683554bcabdc
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-30 14:57:35 +0600

    random data write function

--- quickSortTimeMeasurement.cpp
+++ quickSortTimeMeasurement.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+#define SIZE 10
+int data[SIZE];
+
+void RandomDataWrite(char *FileName){
+    FILE *fp = fopen(FileName,"w");
+    srand(time(NULL));
+
+    for(int i=0;i<SIZE;i++){
+        fprintf(fp,"%d\n",rand()%INT_MAX+1);
+    }
+    fclose(fp);
+}
+
+int main(){
+    RandomDataWrite("RandomIntegers.txt");
+}
