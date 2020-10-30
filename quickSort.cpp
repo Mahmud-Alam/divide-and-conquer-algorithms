1e71191fd6480d8378c75e892edaaf19e8c92c4a
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-30 11:01:27 +0600

    quick sort function write

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,12 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+void quickSort(int arr, int low, int high){
+    if(low<high){
+        int q = Partition(arr,low,high);
+        quickSort(arr,low,q-1);
+        quickSort(arr,q+1,high);
+    }
+}
 
 int main(){
     int arr[] = { 9, 4, 7, 10, 8, 5, 1, 3, 2, 6};
 
-    quickSort(arr,1,n);
+    quickSort(arr,0,n-1);
 
     for(int i=0;i<n;i++){
         cout<<arr[i]<<"\t";
