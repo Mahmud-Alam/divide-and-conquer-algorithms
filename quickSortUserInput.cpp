9ac768ea69c4f2ff7e239d87b9fc40270cf1c91e
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-30 12:02:45 +0600

    main func

--- quickSortUserInput.cpp
+++ quickSortUserInput.cpp
@@ -1,1 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int arr[100],value,i=0;
+    cout<<"Input values(type 1024 to stop): ";
+    while(1){
+        cin>>value;
+        if(value==1024) break;
+        else{
+            arr[i] = value;
+            i++;
+        }
+    }
+    quickSort(arr,0,i-1);
+    cout<<endl;
+    for(int j=0;j<i;j++){
+        cout<<arr[j]<<"\t";
+    }
+    cout<<endl;
+}
