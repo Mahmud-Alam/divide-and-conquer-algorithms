8e4b53f7bae78597e7574edf4fe7fe39c97d9cbe
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-30 23:21:22 +0600

    ternary main func

--- ternarySearch.cpp
+++ ternarySearch.cpp
@@ -1,8 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    cout<<"Input array size: ";cin>>n;
+int ternarySearch(int key, int n, int *arr){
 
+}
+
+int main(){
+    int key, n, arr[] = {1,2,3,4,5,6,7,8,9,11,15,21,36,65,98};
+
+    n = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<"Input Search key: ";cin>>key;
+
+    int value = ternarySearch(key,n,arr);
 }
