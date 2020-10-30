db9d23f9ed23333878405413897f6f0802efab0b
Mahmud-Alam <official.mahmud23@gmail.com>
2020-10-31 01:09:24 +0600

    ERROR SOLVED: some correction in mid1 and mid2 equations

--- ternarySearch.cpp
+++ ternarySearch.cpp
@@ -1,27 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int ternarySearch(int l, int r, int key, int *arr){
-    if(r>=l){
-        int mid1 = l+(r-1)/3;
-        int mid2 = r-(r-1)/3;
+int ternarySearch(int low, int high, int key, int *arr){
+    if(high>=low){
+        int mid1 = low+(high-low)/3;
+        int mid2 = high-(high-low)/3;
 
         if(arr[mid1]==key) return mid1;
         if(arr[mid2]==key) return mid2;
 
         if(key<arr[mid1])
-            return ternarySearch(l,mid-1,key,arr);
+            return ternarySearch(low,mid1-1,key,arr);
         else if(key>arr[mid2])
-            return ternarySearch(mid2+1,r,key,arr);
-        else return ternarySearch(mid1+1,mid2-1,key,arr)
+            return ternarySearch(mid2+1,high,key,arr);
+        else
+            return ternarySearch(mid1+1,mid2-1,key,arr);
     }
     return -1;
 }
 
 int main(){
-    int key, n, arr[] = {1,2,3,4,5,6,7,8,9,11,15,21,36,65,98};
+    int key;
+    int arr[] = {1,2,3,4,5,6,7,8,9,11,15,16,17,18,19};
 
-    n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(arr)/sizeof(arr[0]);
 
     cout<<"Input Search key: ";cin>>key;
 
