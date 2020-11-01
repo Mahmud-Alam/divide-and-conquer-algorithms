bff6e42fea3e73774b366d899b69e4e3d696add9
Mahmud-Alam <official.mahmud23@gmail.com>
2020-11-01 08:56:45 +0600

    binary search function

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int binarySearch(int low, int high, int key, int *arr){
+    if(high>=low){
+        int mid = (low+high)/2;
+
+        if(arr[mid]==key) return mid;
+
+        if(arr[mid]>key) return binarySearch(low,mid-1,key,arr);
+
+        else if(arr[mid]<key) return binarySearch(mid+1,high,key,arr);
+    }
+    return -1;
+}
+
 int main(){
     int arr[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21};
 
