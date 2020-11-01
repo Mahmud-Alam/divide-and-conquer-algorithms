04624d3f10f2f330e015ee8ac85b9446e2cd1460
Mahmud-Alam <official.mahmud23@gmail.com>
2020-11-01 17:26:59 +0600

    main func

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,1 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int arr[] = {9,5,1,3,7,6,4,2,8,10,1,7};
+
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    mergeSort(0,n-1,arr);
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<"  ";
+    }
+    cout<<endl;
+}
