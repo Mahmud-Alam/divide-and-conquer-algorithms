1fc1347be09c5005b07fc5317d24a5fc1666e823
Mahmud-Alam <official.mahmud23@gmail.com>
2020-11-01 20:28:38 +0600

    merge sort done

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -7,6 +7,32 @@ void mergeArray(int low, int mid, int high, int *arr){
 
     int left[n1],right[n2];
 
+    for(int i=0;i<n1;i++)
+        left[i] = arr[low+i];
+    for(int i=0;i<n2;i++)
+        right[i] = arr[mid+1+i];
+    int i=0,j=0,k=low;
+    while(i<n1&&j<n2){
+        if(left[i]<=right[j]){
+            arr[k]=left[i];
+            i++;
+        }
+        else{
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1){
+        arr[k]=left[i];
+        i++;
+        k++;
+    }
+    while(j<n2){
+        arr[k]=right[j];
+        j++;
+        k++;
+    }
 }
 
 void mergeSort(int low,int high, int *arr){
@@ -21,7 +47,7 @@ void mergeSort(int low,int high, int *arr){
 }
 
 int main(){
-    int arr[] = {9,5,1,3,7,6,4,2,8,10,1,7};
+    int arr[] = {12,9,5,16,1,3,7,15,14,12,11,13,15,17,19,6,4,2,8,18,10,20,1,7};
 
     int n = sizeof(arr)/sizeof(arr[0]);
 
